ba3a485e8c32346bbb9310d9f4a045cf79cd65bb
Lucas <lucasferguson16@gmail.com>
2022-02-24 17:35:55 -0600

    AutonomousShootCargo Command Procedure

--- src/main/java/frc/robot/commands/AutonomousShootCargo.java
+++ src/main/java/frc/robot/commands/AutonomousShootCargo.java
@@ -6,6 +6,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Command Procedure
+  1. Start Shooter Motor
+  2. Wait until Shooter Motor is at full speed.
+  3. Wait for vision to see if we are on target (if we have it working)
+ */
 public class AutonomousShootCargo extends CommandBase {
   /** Creates a new AutonomousShootCargo. */
   public AutonomousShootCargo() {
