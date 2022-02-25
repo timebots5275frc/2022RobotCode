eeebc756a97394f8bfc1ac3f1840c236199bcd33
Lucas <lucasferguson16@gmail.com>
2022-02-24 19:51:50 -0600

    Creating GyroWrapperADIS16470_IMU

---
+++ src/main/java/frc/robot/subsystems/drivetrain/GyroWrapperADIS16470_IMU.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.drivetrain;
+import edu.wpi.first.wpilibj.interfaces.*;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+
+public class GyroWrapperADIS16470_IMU extends ADIS16470_IMU implements Gyro{
+    
+}
+
+
+/**
+ * public class gyroWrapper extends ADIS16470_IMU implements Gyro{
+    
+}
+ */
