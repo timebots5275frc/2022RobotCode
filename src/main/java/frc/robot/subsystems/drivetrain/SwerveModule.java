fd34a6791160ae8b7605c9ec91f3e798a1fa3289
Lucas <lucasferguson16@gmail.com>
2022-02-24 18:03:57 -0600

    Adding REVLib 2022.1.1

---
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.drivetrain;
+
+import com.revrobotics.CANSparkMax;
+
+public class SwerveModule {
+    
+    public CANSparkMax driveMotor;
+    private CANSparkMax steerMotor;
+}
