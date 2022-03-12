22f899259c348c4b6d32b9c0fd3cbc7b9040b64d
Allen Voss <allenvoss407@gmail.com>
2022-03-12 10:21:11 -0600

    Intake and Hopper and their commands

--- src/main/java/frc/robot/commands/single_subsystem/RunUpperHopper.java
+++ src/main/java/frc/robot/commands/single_subsystem/RunUpperHopper.java
@@ -2,33 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.multi_subsystem;
+package frc.robot.commands.single_subsystem;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.hopper.Hopper;
-import frc.robot.subsystems.intake.Intake;
 
-/**
- * Command Procedure Button For Intaking Cargo Start IntakeCargo_AutoHopper
- * Command 1. Start Intake
- * Motor 2. If both hopper positions are empty → move both hopper motors 3. If
- * ball in hopper
- * position 2 → move new ball to hooper position 1 4. If a ball is in both
- * positions → the intake
- * motor will be disabled.
- */
-public class IntakeCargo_AutoHopper extends CommandBase {
-    /** Creates a new IntakeCargoAutoHopper. */
-    private Intake intake;
+public class RunUpperHopper extends CommandBase {
     private Hopper hopper;
+    private double direction;
 
-    public IntakeCargo_AutoHopper(Intake _intake, Hopper _hopper) {
-        // Use addRequirements() here to declare subsystem dependencies.
-        intake = _intake;
+    /** Creates a new RunUpperHopper. */
+    public RunUpperHopper(Hopper _hopper, double forwardOrBackward) {
         hopper = _hopper;
-        addRequirements(_intake);
+        direction = forwardOrBackward;
         addRequirements(_hopper);
+        // Use addRequirements() here to declare subsystem dependencies.
     }
 
     // Called when the command is initially scheduled.
@@ -39,13 +27,13 @@ public class IntakeCargo_AutoHopper extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-
+        hopper.runUpperHopper(direction);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-
+        hopper.runUpperHopper(0);
     }
 
     // Returns true when the command should end.
